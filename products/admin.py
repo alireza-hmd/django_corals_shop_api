62e92d6f54391d4ca001edec8580d44db4026713
Alireza Hamedi <alireza12196@gmail.com>
2023-05-01 22:32:21 +0330

    feat: admin page for product models modified

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Product, Brand, Category, Image, Comment
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug')
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'brand', 'price', 'available', 'created_at')
+    list_filter = ('category', 'available', 'created_at')
+    list_editable = ('price', 'available')
+
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'country')
+
+
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('image', 'product')
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('product', 'author', 'active', 'created_at')
+    list_filter = ('active', 'created_at')
+    search_fields = ('body',)
+    list_editable = ('active',)
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Brand, BrandAdmin)
+admin.site.register(Image, ImageAdmin)
+admin.site.register(Comment, CommentAdmin)
