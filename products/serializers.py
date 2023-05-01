bcd0f390a67577176107b43f0532ed5f34db8211
Alireza Hamedi <alireza12196@gmail.com>
2023-05-01 23:58:29 +0330

    feat: products serializers added

---
+++ products/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+from django.core.validators import FileExtensionValidator
+from .models import Product
+
+
+class ProductCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ('title', 'category', 'brand', 'price', 'volume', 'description')
+
+
+class ProductListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ('title', 'brand', 'price', 'available')
+
+
+class ProductDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+class ImageUploadSerializer(serializers.Serializer):
+    image = serializers.ImageField(
+        validators=[FileExtensionValidator(['jpeg', 'jpg', 'png'])]
+    )
+    product_slug = serializers.SlugField()
+
+
+class ImageListSerializer(serializers.Serializer):
+    image_url = serializers.CharField()
+    product_slug = serializers.SlugField()
+
+
